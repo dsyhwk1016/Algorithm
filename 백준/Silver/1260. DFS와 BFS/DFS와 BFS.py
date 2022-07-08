977341c8ebdc2db5b93c82400e6586a702948665
가영 <73643088+dsyhwk1016@users.noreply.github.com>
2022-07-09 00:19:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 728 ms, Memory: 32820 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+n, m, v = map(int, input().split()) # n=정점 수, m=간선 수, v=시작 정점
+
+# 간선 관계로 graph 생성
+graph = [ [] for _ in range(n) ]
+for _ in range(m):
+    s, e = map(int, input().split())
+    graph[s-1].append(e)
+    graph[e-1].append(s)
+for i in range(n): graph[i].sort()  # 오름차순 정렬
+
+
+### DFS ###
+dfsVisited = [] # DFS 방문 리스트
+def DFS(graph, cur):
+    if cur not in dfsVisited:   # 현재 정점을 방문하지 않았다면
+        dfsVisited.append(cur)  # 방문 리스트에 삽입
+        
+        # 현재 정점의 도착 정점들에 대해 재귀
+        for i in graph[cur-1]:
+            DFS(graph, i)
+DFS(graph, v)
+
+
+### BFS ###
+queue = deque([v])  # 첫 시작은 v
+bfsVisited = [v] # BFS 방문 리스트
+
+# queue가 빌 때까지 반복
+while queue:
+    cur = queue.popleft()  # 이번에 방문한 정점
+    
+    # 현재 정점의 도착 정점들에 대해 반복
+    for i in graph[cur-1]:
+        if i not in bfsVisited:
+            # 방문하지 않았다면 삽입
+            bfsVisited.append(i)
+            queue.append(i)
+
+for d in dfsVisited: print(d, end=' ')
+print()
+for b in bfsVisited: print(b, end=' ')
