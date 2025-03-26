e1e0f5b927db34d7ea9df39bdaa188e5716c015a
가영 <73643088+dsyhwk1016@users.noreply.github.com>
2025-03-26 17:45:12 +0900

    [LinkedList] insert 매서드 추가

--- 자료구조/linked-list/linked-list.js
+++ 자료구조/linked-list/linked-list.js
@@ -5,4 +5,38 @@ class Node {
     }
 }
 
-export { Node };
+class LinkedList {
+    constructor() {
+        this.header = null;
+        this.count = 0;
+    }
+
+    insert(index, data) {
+        if (index < 0 || index > this.count) {
+            throw new Error("Invalid index");
+        }
+
+        const newNode = new Node(data);
+
+        if (index === 0) {
+            newNode.next = this.header;
+            this.header = newNode;
+        } else {
+            let currentNode = this.header;
+            for (let i = 1; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+        this.count++;
+    }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.insert(0, 9);
+linkedList.insert(1, 7);
+linkedList.insert(2, 6);
+linkedList.insert(3, 5);
