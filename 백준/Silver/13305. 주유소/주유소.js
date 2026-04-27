e672416367e1a3eb46bda546bd61356613a434ee
가영 <73643088+dsyhwk1016@users.noreply.github.com>
2026-04-27 14:08:02 +0900

    [Silver III] Title: 주유소, Time: 376 ms, Memory: 81332 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -11,29 +11,23 @@ rl.on("line", (line) => {
     if (!n) {
         n = Number(line);
     } else if (!roadList?.length) {
-        roadList = line.split(" ").map(distance => [Number(distance)]);
+        roadList = line.split(" ").map(distance => [BigInt(distance)]);
     } else {
-        line.split(" ").slice(0, n - 1).forEach((price, index) => roadList[index].push(Number(price)));
+        line.split(" ").slice(0, n - 1).forEach((price, index) => roadList[index].push(BigInt(price)));
 
         rl.close();
     }
 }).on("close", () => {
-    let totalPrice = 0;
-    let minPrice = roadList[0][1];
-    let totalDistance = 0;//roadList[0][0];
+    let totalPrice = 0n;
+    let minPrice = BigInt(roadList[0][1]);
     for (const [distance, price] of roadList) {
         if (price < minPrice) {
-            totalPrice += minPrice * totalDistance;
-            totalDistance = distance;
             minPrice = price;
-            continue;
         }
 
-        totalDistance += distance;
+        totalPrice += minPrice * distance;
     }
-    totalPrice += minPrice * totalDistance;
-
-    console.log(totalPrice);
+    console.log(totalPrice.toString());
 
     process.exit();
 });
