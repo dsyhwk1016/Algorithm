db4e70b5f5422d4a3740562bbf78fc0b7b68ffce
가영 <73643088+dsyhwk1016@users.noreply.github.com>
2026-05-12 14:27:19 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,4 @@
 function solution(nums) {
-    const map = new Map();
-    nums.forEach(num => map.set(num, (map.get(num) ?? 0) + 1));
-
-    return Math.min(nums.length / 2, map.size);
+    const set = new Set(nums);
+    return Math.min(nums.length / 2, set.size);
 }
